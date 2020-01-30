e8965fa2878989e1d5ec6d1449d191434c9b4ee7
AlbertDiaz2 <albertedinson52@gmail.com>
2020-01-30 18:06:35 -0400

    Casos variados de requisitos

--- test.py
+++ test.py
@@ -33,5 +33,45 @@ class PensionTest(unittest.TestCase):
 		semanas = 750
 		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'tiene Pension, no deberia')
 
+	def testHombreNoCumpleSemanasRequisito(self):
+		sexo = 'm'
+		anhosServicio = 62
+		condicion = 0
+		reduccion = 0
+		semanas = 645
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'No tiene Pension')
+
+	def testMujerNoCumpleEdadRequisito(self):
+		sexo = 'h'
+		anhosServicio = 52
+		condicion = 0
+		reduccion = 0
+		semanas = 751
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'No tiene Pension')
+
+	def testMujerNoCumpleSemanasRequisito(self):
+		sexo = 'h'
+		anhosServicio = 59
+		condicion = 0
+		reduccion = 0
+		semanas = 733
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'No tiene Pension')
+
+	def testHombreNoCumpleAmbosRequisito(self):
+		sexo = 'm'
+		anhosServicio = 47
+		condicion = 0
+		reduccion = 0
+		semanas = 745
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'No tiene Pension')
+
+	def testMujerNoCumpleAmbosRequisito(self):
+		sexo = 'h'
+		anhosServicio = 59
+		condicion = 0
+		reduccion = 0
+		semanas = 733
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'No tiene Pension')
+
 if __name__ == '__main__':
 	unittest.main()
