7247fbea5014aa146a8b23d435b8c58246060b6b
AlbertDiaz2 <albertedinson52@gmail.com>
2020-01-30 15:24:44 -0400

    Primera verificacion

--- pension.py
+++ pension.py
@@ -1,4 +1,12 @@
+from datetime import date
+
 class Pension():
 
 	def __init__(self):
 		pass
+
+	def verificar(self,sexo, anhosServicio, semanas, condicion):
+		if (anhosServicio >= 60 and sexo == 'm' and condicion == 0 and semanas >= 750):
+			return True
+		else:
+			pass
