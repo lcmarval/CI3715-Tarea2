5658449ea07f7c28eec13be8e8c57aa5d824b6e2
Luis Marval <luiscarm77@gmail.com>
2020-01-30 19:58:11 -0400

    Articulo 162 Falsos y Casos Maliciosos

--- test.py
+++ test.py
@@ -83,7 +83,56 @@ class PensionTest(unittest.TestCase):
 		semanas = 750
 		self.assertTrue(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'No tiene Pension')
 
+	# Artuclo 162 falso
+	def testHombreCumpleConReduccionRequisitoAnhosInsuficientes(self):
+		sexo = 'm'
+		anhosServicio = 53
+		condicion = 5
+		semanas = 750
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'Tiene Pension, No debe')
+
+	def testMujerConReduccionArticulo162AnhosInsuficientes(self):
+		sexo = 'f'
+		anhosServicio = 40
+		condicion = 1
+		semanas = 750
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'Tiene Pension, no debe')
+
 	# Casos Maliciosos
+	def testHombreMayuscula(self):
+		self.assertFalse(self.g.verificar('M', 60, 750, 0), 'Tiene pension, es Mayuscula')
+
+	def testMujerMayuscula(self):
+		self.assertFalse(self.g.verificar('F', 55, 750, 0), 'Tiene pension, es Mayuscula')
+
+	def testParametroSexoIncorrecto(self):
+		sexo = 'n'
+		anhosServicio = 59
+		condicion = 1
+		semanas = 750
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'Tiene pension, sin saber sexo')
+
+	def testParametroAnhosServicioIncorrecto(self):
+		sexo = 'm'
+		anhosServicio = 'sesenta'
+		condicion = 1
+		semanas = 750
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'Tiene pension, Anhos tipo incorrecto')
+	
+	def testParametroCondicionFloatIncorrecto(self):
+		sexo = 'f'
+		anhosServicio = 54
+		condicion = 1.1 # incorrecto ?
+		semanas = 750
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'Tiene pension, Condicion tipo incorrecto')
+
+	def testParametroSemanasRedondeaIncorrecto(self):
+		sexo = 'f'
+		anhosServicio = 56
+		condicion = 0
+		semanas = 749.6
+		self.assertFalse(self.g.verificar(sexo, anhosServicio, semanas, condicion), 'Tiene pension, Redondea semanas tipo incorrecto')
+
 
 if __name__ == '__main__':
 	unittest.main()
